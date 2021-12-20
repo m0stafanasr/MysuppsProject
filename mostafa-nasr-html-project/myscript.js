9f57d8b0de0124ba263c63228e3f695a1a681f62
esraa <esraa.mohamed.fcis@gmail.com>
2021-12-21 00:13:01 +0200

    third

--- mostafa-nasr-html-project/myscript.js
+++ mostafa-nasr-html-project/myscript.js
@@ -1,3 +1,27 @@
+
+function submit(){
+    var user = document.getElementById("username").value;
+var pass = document.getElementById("password").value;
+var d = new Date();
+d.setMonth(d.getMonth()+3);
+document.cookie = "userName="+ user+"; expires="+d;
+document.cookie =    "password="+pass+"; expires="+d;
+if(document.getElementById("check").checked){
+    localStorage.setItem("username", user);
+    localStorage.setItem("password", pass);
+    console.log(user)
+}
+}
+
+function show(){
+    document.getElementById("loginbar").style.display = "block";
+}
+
+function hide(){
+    document.getElementById("loginbar").style.display = "none";
+   
+}
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "products.json");
 xhr.onreadystatechange= function(){
@@ -46,25 +70,4 @@ function Display(jsobj){
         boxs.appendChild(box);
     }
 
-function submit(){
-    var user = document.getElementById("username").value;
-var pass = document.getElementById("password").value;
-var d = new Date();
-d.setMonth(d.getMonth()+3);
-document.cookie = "userName="+ user+"; expires="+d;
-document.cookie =    "password="+pass+"; expires="+d;
-if(document.getElementById("check").checked){
-    localStorage.setItem("username", user);
-    localStorage.setItem("password", pass);
-    console.log(user)
 }
-}
-
-function show(){
-    document.getElementById("loginbar").style.display = "block";
-}
-
-function hide(){
-    document.getElementById("loginbar").style.display = "none";
-   
-}}
